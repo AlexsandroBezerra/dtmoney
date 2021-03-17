7300552001584446b224e89b173ef652ca0e8162
Alexsandro G Bezerra <alexsandro.g.bezerra@gmail.com>
2021-03-17 20:13:35 -0300

    feat: add summary

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
+
 import { GlobalStyle } from './styles/global'
 
 export function App() {
   return (
     <>
       <GlobalStyle />
+
       <Header />
+      <Dashboard />
     </>
   );
 }
